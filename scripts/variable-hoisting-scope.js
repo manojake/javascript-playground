abd28925c664b608013fa5302a5f9bbc4fedb84f
Manosh Jacob <68778250+manojake@users.noreply.github.com>
2024-05-31 08:13:54 -0400

    Update variable-hoisting-scope.js

--- scripts/variable-hoisting-scope.js
+++ scripts/variable-hoisting-scope.js
@@ -1,19 +1,41 @@
 
 //   ****************** VAR ******************
+// console.log(w); // ReferenceError: w is not defined
+// console.log(g); // undefined
+// var g = 5;
+// console.log(g); // 5
+// var g;
+// console.log(g); // 5
+// g = 6;
+// console.log(g); // 6
+
+// (function () {
+//   console.log(g); // undefined
+//   var g = "local value";
+//   var w = 7;
+//   console.log(g); // local value
+// })();
+
+// console.log(g); // 6
+
+// var d = 1;
+// var d = 2;
+
+// console.log(d); // 2
 
 // Var variables are hoisted to the top of the function scope or global scope
 // even if its declaration isn't reached yet. However, if you access a variable before it's declared,
 // the value is always undefined, because only its declaration is hoisted, but not its initialization.
 
-console.log(x === undefined); // true
-var x = 3;
-console.log(x); // 3
+// console.log(x === undefined); // true
+// var x = 3;
+// console.log(x); // 3
 
-(function () {
-  console.log(x); // undefined
-  var x = "local value";
-  console.log(x); // local value
-})();
+// (function () {
+//   console.log(x); // undefined
+//   var x = "local value";
+//   console.log(x); // local value
+// })();
 
 // The above example will be interpreted the same as:
 // var x;
@@ -22,7 +44,7 @@ console.log(x); // 3
 // console.log(x); // 3
 
 // (function () {
-// all var statements in a function should be placed as near to the top of the function as possible
+// // all var statements in a function should be placed as near to the top of the function as possible
 //   var x; 
 //   console.log(x); // undefined
 //   x = "local value";
@@ -45,30 +67,31 @@ console.log(x); // 3
 
 // If no initial value was specified with the variable declaration, 
 // it will be initialized with a value of undefined.
-try {
-  (function () {
-    // TDZ starts at beginning of scope
-    console.log(bar); // "undefined"
-    // console.log(foo); // ReferenceError: Cannot access 'foo' before initialization
-    var bar = 1;
-    let foo = 2; // End of TDZ (for foo)
-  })();
-} catch(e) {
-  console.log(e);
-}
+// try {
+//   (function () {
+//     // TDZ starts at beginning of scope
+//     console.log(bar); // "undefined"
+//     // console.log(foo); // ReferenceError: Cannot access 'foo' before initialization
+//     var bar = 1;
+//     let foo = 2; // End of TDZ (for foo)
+//     let foo = 3; //SyntaxError: Identifier 'foo' has already been declared
+//   })();
+// } catch(e) {
+//   console.log(e);
+// }
 
 // The term "temporal" is used because the zone depends on the order of execution (time) rather than 
 // the order in which the code is written (position)
 
-(function () {
-  // TDZ starts at beginning of scope
-  const func = () => console.log(letVar); // OK
+// (function () {
+//   // TDZ starts at beginning of scope
+//   const func = () => console.log(letVar); // OK
 
-  // Within the TDZ letVar access throws `ReferenceError`
+//   // Within the TDZ letVar access throws `ReferenceError`
 
-  let letVar = 3; // End of TDZ (for letVar)
-  func(); // Called outside TDZ!
-})();
+//   let letVar = 3; // End of TDZ (for letVar)
+//   func(); // Called outside TDZ!
+// })();
 
 
 
@@ -101,46 +124,46 @@ try {
 // To avoid the error, wrap each case in a new block statement.
 
 
-switch (x) {
-  case 0: {
-    let foo;
-    break;
-  }
-  case 1: {
-    let foo;
-    break;
-  }
-}
+// switch (x) {
+//   case 0: {
+//     let foo;
+//     break;
+//   }
+//   case 1: {
+//     let foo;
+//     break;
+//   }
+// }
 
 
 
 //  ****************** SCOPING ******************
 // console.log(blah); // ReferenceError: blah is not defined
 
-function varTest() {
-  var blah = 1;
-  {
-    var blah = 2; // same variable!
-    console.log(blah); // 2
-  }
-  console.log(blah); // 2
-}
+// function varTest() {
+//   var blah = 1;
+//   {
+//     var blah = 2; // same variable!
+//     console.log(blah); // 2
+//   }
+//   console.log(blah); // 2
+// }
 
-function letTest() {
-  let blah = 1;
-  {
-    let blah = 2; // different variable
-    console.log(blah); // 2
-  }
-  console.log(blah); // 1
-}
+// function letTest() {
+//   let blah = 1;
+//   {
+//     let blah = 2; // different variable
+//     console.log(blah); // 2
+//   }
+//   console.log(blah); // 1
+// }
 
 
 
 // console.log(blum); // ReferenceError: blum is not defined
-(function() {
-  var blum = 2;
-})();
+// (function() {
+//   var blum = 2;
+// })();
 
 
 
@@ -161,27 +184,27 @@ function letTest() {
 // test1();
 
 
-function test2() {
-  var foo = 33;
-  if (foo) {
-    var foo =  55; 
-    console.log(foo); // 55
-  }
-  console.log(foo); // 55
-}
-test2();
+// function test2() {
+//   var foo = 33;
+//   if (foo) {
+//     var foo =  55; 
+//     console.log(foo); // 55
+//   }
+//   console.log(foo); // 55
+// }
+// test2();
 
 
 
-function test3() {
-  var foo = 33;
-  if (foo) {
-    let foo =  55; 
-    console.log(foo); // 55
-  }
-  console.log(foo); // 33
-}
-test3();
+// function test3() {
+//   var foo = 33;
+//   if (foo) {
+//     let foo =  55; 
+//     console.log(foo); // 55
+//   }
+//   console.log(foo); // 33
+// }
+// test3();
 
 
 
@@ -191,10 +214,10 @@ this value is not available inside the block: the identifier foo inside the if b
 The expression foo + 55 throws a ReferenceError because initialization of let foo has not completed
  — it is still in the temporal dead zone.
 */
-function test() {
-  var foo = 33;
-  if (foo) {
-    let foo = foo + 55; // ReferenceError Cannot access 'foo' before initialization
-  }
-}
-test();
+// function test() {
+//   var foo = 33;
+//   if (foo) {
+//     let foo = foo + 55; // ReferenceError Cannot access 'foo' before initialization
+//   }
+// }
+// test();
